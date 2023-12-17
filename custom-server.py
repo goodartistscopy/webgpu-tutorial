d8990cca30549a29449a896642648e3d28b0ec24
Christophe Dehais <christophe.dehais@gmail.com>
2023-12-18 00:11:02 +0100

    Add script to serve cross origin headers

---
+++ custom-server.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+# hacked server that just add cross origin headers for workers
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        super().end_headers()
+
+def run(server_class=HTTPServer, handler_class=CustomHandler, port=6969):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server running on port {port}")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
