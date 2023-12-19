f5aacfd6b4db5db4aa131abad493dc8b5d00e889
Christophe Dehais <christophe.dehais@gmail.com>
2023-12-19 10:27:00 +0100

    Add benchmark to the GPU compute version

--- prefix_sum/main.js
+++ prefix_sum/main.js
@@ -1,5 +1,5 @@
 import { prefixSum, prefixSumInPlace, roundToDigits, createWorkers, prefixSumParallel } from "./common.js";
-import { initContext, loadTextFiles } from "../init/context.js"
+import { initContext, loadTextFiles } from "../init/context.js";
 
 async function benchmark(workload, numIters, numWarmups = 0) {
     for (let i = 0; i < numWarmups; i++) {
@@ -10,13 +10,14 @@ async function benchmark(workload, numIters, numWarmups = 0) {
         await workload();
         performance.measure("bench-dur", "bench-start");
     }
-    let mean = 0, stddev = 0;
+    let mean = 0,
+        stddev = 0;
     performance.getEntriesByType("measure").forEach((entry) => {
         mean += entry.duration;
         stddev += entry.duration * entry.duration;
     });
     mean /= numIters;
-    stddev = Math.sqrt((stddev / numIters) - mean * mean);
+    stddev = Math.sqrt(stddev / numIters - mean * mean);
 
     performance.clearMeasures();
     performance.clearMarks();
@@ -25,9 +26,9 @@ async function benchmark(workload, numIters, numWarmups = 0) {
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function buildRandomArray(length) {
@@ -40,7 +41,7 @@ function fillArrayWithRandom(array) {
     }
 }
 
-const numElements = 24234; // maximum is 65792 for now;
+const numElements = 1e6; // maximum is 65792 for now;
 let arrayIn = buildRandomArray(numElements);
 let arrayOut = prefixSum(arrayIn);
 
@@ -48,57 +49,108 @@ let numRuns = 100;
 let numWarmups = 20;
 let bench = await benchmark(() => prefixSumInPlace(arrayIn, arrayOut), numRuns, numWarmups);
 let throughput = arrayIn.length / (1e6 * bench.mean * 1e-3);
-console.log(`Sequential: ${roundToDigits(bench.mean, 3)}ms (+/- ${roundToDigits(bench.stddev, 2)}) ${roundToDigits(throughput, 1)} Melem.s^-1`);
+console.log(
+    `Sequential: ${roundToDigits(bench.mean, 3)}ms (+/- ${roundToDigits(bench.stddev, 2)}) ${roundToDigits(
+        throughput,
+        1,
+    )} Melem.s^-1`,
+);
 
-// console.log(`in : ${arrayIn}`);
-// console.log(`out: ${arrayOut}`);
+//console.log(`in : ${arrayIn}`);
+//console.log(`out: ${arrayOut}`);
 
 const maxNumThreads = 16;
 
-// let bufferIn = new SharedArrayBuffer(4 * numElements);
-// arrayIn = new Int32Array(bufferIn, 0, numElements);
-// fillArrayWithRandom(arrayIn);
+let bufferIn = new SharedArrayBuffer(4 * numElements);
+arrayIn = new Int32Array(bufferIn, 0, numElements);
+fillArrayWithRandom(arrayIn);
 
 //console.log(`in : ${arrayIn}`);
 
-// let bufferOut = new SharedArrayBuffer(4 * numElements);
-// arrayOut = new Int32Array(bufferOut);
+let bufferOut = new SharedArrayBuffer(4 * numElements);
+arrayOut = new Int32Array(bufferOut);
 
 let workers = createWorkers("./worker.js", maxNumThreads);
 
-// bench = await benchmark(async () => { await prefixSumParallel(arrayIn, arrayOut, workers); }, numRuns, numWarmups);
-// throughput = arrayIn.length / (1e6 * bench.mean * 1e-3);
-// console.log(`Parallel: ${roundToDigits(bench.mean, 3)}ms (+/- ${roundToDigits(bench.stddev, 2)}) ${roundToDigits(throughput, 1)} Melem.s^-1`);
+bench = await benchmark(
+    async () => {
+        await prefixSumParallel(arrayIn, arrayOut, workers);
+    },
+    numRuns,
+    numWarmups,
+);
+throughput = arrayIn.length / (1e6 * bench.mean * 1e-3);
+console.log(
+    `Parallel: ${roundToDigits(bench.mean, 3)}ms (+/- ${roundToDigits(bench.stddev, 2)}) ${roundToDigits(
+        throughput,
+        1,
+    )} Melem.s^-1`,
+);
 
 //console.log(`out: ${arrayOut}`);
 
-// GPU code is designed around blocks of 256 elements
-const numSlices = Math.ceil(numElements / 256);
-const numElementsPadded = numSlices * 256;
+// GPU code is designed around blocks of sliceLength elements
+const sliceLength = 1024;
+const numSlices = Math.ceil(numElements / sliceLength);
+const numSlicesPadded = numSlices * sliceLength;
+const numElementsPadded = numSlices * sliceLength;
 
 const shaderFiles = ["prefix_sum.wgsl"];
 
-const wgpu = await initContext(null); //, ["timestamp-query"]);
-let {device, canvas, context, adapter, ...rest} = wgpu;
+let extendedLimits = {
+    maxComputeInvocationsPerWorkgroup: 1024,
+    maxComputeWorkgroupSizeX: 1024,
+    maxComputeWorkgroupSizeY: 1024,
+};
+const wgpu = await initContext(null, ["timestamp-query"], extendedLimits);
+let { device, canvas, context, adapter, ...rest } = wgpu;
 
 let shaders = await loadTextFiles(shaderFiles);
 
-let module = device.createShaderModule({ code: shaders["prefix_sum.wgsl"]});
+let module = device.createShaderModule({ code: shaders["prefix_sum.wgsl"] });
 
-let srcBuffer = device.createBuffer({ size: 4 * numElementsPadded, usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE });
+//== Timestamp queries for benchmarking
+let tsSet;
+let tsBuffers = [];
+if (device.features.has("timestamp-query")) {
+    tsSet = device.createQuerySet({ count: 2, type: "timestamp" });
+    // one buffer where the queries will resolve into, one to read the result back to host
+    tsBuffers[0] = device.createBuffer({ size: 16, usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.QUERY_RESOLVE });
+    tsBuffers[1] = device.createBuffer({ size: 16, usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ });
+}
+
+let srcBuffer = device.createBuffer({
+    size: 4 * numElementsPadded,
+    usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE,
+});
 device.queue.writeBuffer(srcBuffer, 0, arrayIn);
-let dstBuffer = device.createBuffer({ size: 4 * numElementsPadded, usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.STORAGE });
+let dstBuffer = device.createBuffer({
+    size: 4 * numElementsPadded,
+    usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.STORAGE,
+});
 // buffers for storing the partial sums of the slices and the prefix sum of them
-let partialBuffer = device.createBuffer({ size: 4 * numSlices, usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE });
-let partialSumBuffer = device.createBuffer({ size: 4 * numSlices, usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.STORAGE });
-let rbBuffer = device.createBuffer({ size: 4 * numElementsPadded, usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ });
+let partialBuffer = device.createBuffer({
+    size: 4 * numSlices,
+    usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE,
+});
+let partialSumBuffer = device.createBuffer({
+    size: 4 * numSlices,
+    usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.STORAGE,
+});
+let rbBuffer = device.createBuffer({
+    size: 4 * numElementsPadded,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
+});
 
 let pipeline = device.createComputePipeline({
     compute: {
         module,
         entryPoint: "prefix_sum_hillis_steele",
+        constants: {
+            slice_length: sliceLength,
+        },
     },
-    layout: "auto"
+    layout: "auto",
 });
 let bindGroup = device.createBindGroup({
     entries: [
@@ -112,16 +164,16 @@ let copyPipeline = device.createComputePipeline({
     compute: {
         module,
         entryPoint: "strided_copy",
+        constants: {
+            slice_length: sliceLength,
+        },
     },
-    layout: "auto"
+    layout: "auto",
 });
-let strideBuffer = device.createBuffer({ size: 4, usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM });
-device.queue.writeBuffer(strideBuffer, 0, Uint32Array.from([256]));
 let copyBindGroup = device.createBindGroup({
     entries: [
         { binding: 0, resource: { buffer: dstBuffer } },
         { binding: 1, resource: { buffer: partialBuffer } },
-        { binding: 2, resource: { buffer: strideBuffer } },
     ],
     layout: copyPipeline.getBindGroupLayout(0),
 });
@@ -137,8 +189,11 @@ let offsetPipeline = device.createComputePipeline({
     compute: {
         module,
         entryPoint: "offset_slices",
+        constants: {
+            slice_length: sliceLength,
+        },
     },
-    layout: "auto"
+    layout: "auto",
 });
 let offsetBindGroup = device.createBindGroup({
     entries: [
@@ -148,13 +203,15 @@ let offsetBindGroup = device.createBindGroup({
     layout: offsetPipeline.getBindGroupLayout(0),
 });
 
+performance.mark("gpu-start");
 let encoder = device.createCommandEncoder();
+encoder.writeTimestamp(tsSet, 0);
 
 //== prefix sum of slices
 let pass = encoder.beginComputePass();
 pass.setPipeline(pipeline);
 pass.setBindGroup(0, bindGroup);
-pass.dispatchWorkgroups(numElementsPadded / 256);
+pass.dispatchWorkgroups(numElementsPadded / sliceLength);
 pass.end();
 
 if (numSlices > 1) {
@@ -162,33 +219,59 @@ if (numSlices > 1) {
     pass = encoder.beginComputePass();
     pass.setPipeline(copyPipeline);
     pass.setBindGroup(0, copyBindGroup);
-    pass.dispatchWorkgroups(Math.ceil(numSlices / 256));
+    pass.dispatchWorkgroups(Math.ceil(numSlices / sliceLength));
     pass.end();
 
     // Prefix sum of the partial sums
     pass = encoder.beginComputePass();
     pass.setPipeline(pipeline);
     pass.setBindGroup(0, partialBindGroup);
-    pass.dispatchWorkgroups(Math.ceil(numSlices / 256));
+    pass.dispatchWorkgroups(Math.ceil(numSlices / sliceLength));
     pass.end();
 
     // Offset the slices by the partial sums
     pass = encoder.beginComputePass();
     pass.setPipeline(offsetPipeline);
     pass.setBindGroup(0, offsetBindGroup);
-    pass.dispatchWorkgroups(numElementsPadded / 256);
+    pass.dispatchWorkgroups(numElementsPadded / sliceLength);
     pass.end();
 }
 //encoder.copyBufferToBuffer(dstBuffer, 0, rbBuffer, 0, 4 * numSlices);
 encoder.copyBufferToBuffer(dstBuffer, 0, rbBuffer, 0, 4 * numElements);
 
+encoder.writeTimestamp(tsSet, 1);
+encoder.resolveQuerySet(tsSet, 0, 2, tsBuffers[0], 0);
+encoder.copyBufferToBuffer(tsBuffers[0], 0, tsBuffers[1], 0, 16);
+
 device.queue.submit([encoder.finish()]);
+performance.mark("gpu-submit");
 
 await rbBuffer.mapAsync(GPUMapMode.READ);
 let gpuArrayOut = new Int32Array(rbBuffer.getMappedRange(0, 4 * numElements)).slice();
 rbBuffer.unmap();
-// console.log(arrayOut.length);
-// console.log(gpuArrayOut.length);
-// console.log(`arrayOut: ${gpuArrayOut}`);
-console.log(`zero? = ${Array.from(gpuArrayOut).map((element, index) => ([element, arrayOut[index]])).map(([x1, x2]) => x1 - x2).reduce((acc,val) => acc + val, 0)}`);
+performance.mark("gpu-end");
 
+performance.measure("gpu-submit", "gpu-start", "gpu-submit");
+performance.measure("gpu-readback", "gpu-submit", "gpu-end");
+let gpuSubmitDuration = performance.getEntriesByName("gpu-submit", "measure")[0].duration;
+let gpuReadBackDuration = performance.getEntriesByName("gpu-readback", "measure")[0].duration;
+
+await tsBuffers[1].mapAsync(GPUMapMode.READ);
+let ts = new BigInt64Array(tsBuffers[1].getMappedRange()).slice();
+tsBuffers[1].unmap();
+let gpuExecDuration = Number(ts[1] - ts[0]) / 1e6;
+
+console.log(
+    `GPU compute: submit: ${roundToDigits(gpuSubmitDuration, 3)} ms, readback: ${roundToDigits(
+        gpuReadBackDuration,
+        3,
+    )} ms, execution: ${roundToDigits(gpuExecDuration, 3)} ms`,
+);
+
+//console.log(`arrayOut: ${gpuArrayOut}`);
+console.log(
+    `diff = ${Array.from(gpuArrayOut)
+        .map((element, index) => [element, arrayOut[index]])
+        .map(([x1, x2]) => x1 - x2)
+        .reduce((acc, val) => acc + val, 0)}`,
+);
--- prefix_sum/prefix_sum.wgsl
+++ prefix_sum/prefix_sum.wgsl
@@ -1,53 +1,52 @@
 @group(0) @binding(0) var<storage, read_write> src: array<i32>;
 @group(0) @binding(1) var<storage, read_write> dst: array<i32>;
 
-const d: u32 = 256;
+override slice_length: u32 = 256;
 // dual partitioned temporary array
-var<workgroup> temp: array<i32, 2 * d>;
+var<workgroup> temp: array<i32, 2 * slice_length>;
 
-@compute @workgroup_size(d)
+@compute @workgroup_size(slice_length)
 fn prefix_sum_hillis_steele(
     @builtin(local_invocation_id) thread_id: vec3<u32>,
     @builtin(global_invocation_id) global_id: vec3<u32>
     )
 {
-    // ping-pong between two arrays
+    // ping-pong between the upper and lower partitions of the array
     var p_in:u32 = 0;
-    var p_out:u32 = d;
+    var p_out:u32 = slice_length;
     temp[p_in + thread_id.x] = src[global_id.x];
+
     workgroupBarrier();
 
-    for (var offset = 1u; offset < d; offset *= 2u) {
-        
+    for (var offset = 1u; offset < slice_length; offset *= 2u) {
+
         if (thread_id.x < offset) {
             temp[p_out + thread_id.x] = temp[p_in + thread_id.x];
         } else {
             temp[p_out + thread_id.x] = temp[p_in + thread_id.x] + temp[p_in + thread_id.x - offset];
         }
-        
+
         workgroupBarrier();
-        
+
         // swap temp buffer partitions
-        p_in = d - p_in;
-        p_out = d - p_out;
+        p_in = slice_length - p_in;
+        p_out = slice_length - p_out;
     }
 
     dst[global_id.x] = temp[p_in + thread_id.x];
 }
 
-@group(0) @binding(2) var<uniform> stride: u32;
-
-@compute @workgroup_size(256)
+@compute @workgroup_size(slice_length)
 fn strided_copy(@builtin(global_invocation_id) global_id: vec3<u32>) {
     if (global_id.x < arrayLength(&dst)) {
-        let idx = (global_id.x + 1) * stride - 1;
+        let idx = (global_id.x + 1) * slice_length - 1;
         dst[global_id.x] = src[idx];
     }
 }
 
 @group(0) @binding(2) var<storage, read_write> partialSums: array<i32>;
 
-@compute @workgroup_size(256)
+@compute @workgroup_size(slice_length)
 fn offset_slices(
     @builtin(global_invocation_id) global_id: vec3<u32>,
     @builtin(workgroup_id) slice_id: vec3<u32>
